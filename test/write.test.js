3ef4c42219b569598cdef260333685e19fd4fd74
Dan Barela <daniel.barela@gmail.com>
2016-10-31 08:48:32 -0600

    write tests works

--- test/write.test.js
+++ test/write.test.js
@@ -1,14 +1,37 @@
-require('sqlite3').verbose();
+// require('sqlite3').verbose();
 
 var fs = require('fs');
 var tape = require('tape');
 var MBTiles = require('..');
+var async = require('async');
 var fixtureDir = __dirname + '/fixtures/output';
 
 if (typeof(process) !== 'undefined' && process.version) {
   // Recreate output directory to remove previous tests.
   try { fs.unlinkSync(fixtureDir + '/write_1.mbtiles'); } catch(err) {}
   try { fs.mkdirSync(fixtureDir, 0755); } catch(err) {}
+}
+
+
+function loadTile(tilePath, callback) {
+  if (typeof(process) !== 'undefined' && process.version) {
+    fs.readFile(tilePath, callback);
+  } else {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', tilePath, true);
+    xhr.responseType = 'arraybuffer';
+
+    xhr.onload = function(e) {
+      if (xhr.status !== 200) {
+        return callback();
+      }
+      return callback(null, new Buffer(this.response));
+    };
+    xhr.onerror = function(e) {
+      return callback();
+    };
+    xhr.send();
+  }
 }
 
 tape('test mbtiles file creation', function(assert) {
@@ -31,34 +54,39 @@ tape('test mbtiles file creation', function(assert) {
             // Flip Y coordinate because file names are TMS, but .putTile() expects XYZ.
             coords[2] = Math.pow(2, coords[3]) - 1 - coords[2];
 
-            var tile = fs.readFileSync(__dirname + '/fixtures/images/' + file);
-            mbtiles.putTile(coords[3] | 0, coords[1] | 0, coords[2] | 0, tile, function(err) {
-                if (err) throw err;
-                completed.written++;
-                if (completed.written === 285) {
-                    mbtiles.stopWriting(function(err) {
-                        completed.stopped = true;
-                        if (err) throw err;
-                        verifyWritten();
-                    });
-                }
+            // fs.readFileSync(__dirname + '/fixtures/images/' + file);
+            loadTile(__dirname + '/fixtures/images/' + file, function(err, tile) {
+              mbtiles.putTile(coords[3] | 0, coords[1] | 0, coords[2] | 0, tile, function(err) {
+                  if (err) throw err;
+                  completed.written++;
+                  if (completed.written === 285) {
+                      mbtiles.stopWriting(function(err) {
+                          completed.stopped = true;
+                          if (err) throw err;
+                          verifyWritten();
+                      });
+                  }
+              });
             });
         }
 
         function verifyWritten() {
-            fs.readdirSync(__dirname + '/fixtures/images/').forEach(function(file) {
-                var coords = file.match(/^plain_1_(\d+)_(\d+)_(\d+).png$/);
-                if (coords) {
-                    // Flip Y coordinate because file names are TMS, but .getTile() expects XYZ.
-                    coords[2] = Math.pow(2, coords[3]) - 1 - coords[2];
-                    mbtiles.getTile(coords[3] | 0, coords[1] | 0, coords[2] | 0, function(err, tile) {
-                        if (err) throw err;
-                        assert.deepEqual(tile, fs.readFileSync(__dirname + '/fixtures/images/' + file));
-                        completed.read++;
-                        if (completed.read === 285) assert.end();
+          var images = fs.readdirSync(__dirname + '/fixtures/images/');
+          async.eachSeries(images, function(file, callback) {
+            var coords = file.match(/^plain_1_(\d+)_(\d+)_(\d+).png$/);
+            if (coords) {
+                // Flip Y coordinate because file names are TMS, but .getTile() expects XYZ.
+                coords[2] = Math.pow(2, coords[3]) - 1 - coords[2];
+                loadTile(__dirname + '/fixtures/images/' + file, function(err, expectedTile) {
+                  mbtiles.getTile(coords[3] | 0, coords[1] | 0, coords[2] | 0, function(err, tile) {
+                        assert.deepEqual(tile, expectedTile);
+                        callback(err);
                     });
-                }
-            });
+                });
+            }
+          }, function done() {
+            assert.end();
+          });
         }
     });
 });
